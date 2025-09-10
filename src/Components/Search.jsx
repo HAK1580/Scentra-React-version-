7c80424e54f8da40f07722390be087bc4a2eeed1
HAK1580 <aliofficial1580630@gmail.com>
2025-09-10 20:38:12 +0500

    learning and testing responsive tips

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -3,6 +3,18 @@ import './Search.css'
 import { useState } from 'react'
 
 const Search = ({search,setSearch,hero,setHero}) => {
+  
+  function handleKeyDown(e){
+if(e.key==="Enter"){
+  alert("add products first!");
+
+}
+  }
+
+
+  function handleClick(){
+    alert("add products first!");
+  }
 
   function handleCross(){
     
@@ -14,10 +26,10 @@ const Search = ({search,setSearch,hero,setHero}) => {
     <>
  <div className="search-box-div">
   <div className="search-box">
- <input className='search-here' type="search" placeholder='Search'/>
+ <input onKeyDown={handleKeyDown} className='search-here' type="search" placeholder='Search'/>
  <div className="search-icons">
 
- <img width={35} src="/IMGS/icons/search.svg" alt="" />
+ <img onClick={handleClick} width={35} src="/IMGS/icons/search.svg" alt="" />
  <img className='cross-img'  onClick={handleCross}  src="/IMGS/icons/cross.svg" alt="" />
  </div>
 
